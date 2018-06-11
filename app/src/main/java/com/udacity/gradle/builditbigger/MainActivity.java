4f96838cc424d8a8cdbf4694cab3163d41a7bc34
ahmedsalama <ahmed.salamay@gmail.com>
2018-06-11 09:02:31 +0200

    add java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.phantom.asalama.javajokes.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Joker mJoker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mJoker=new Joker();
     }
 
 
@@ -40,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this,mJoker.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javaJokes/src/main/java/com/phantom/asalama/javajokes/Joker.java
@@ -0,0 +1,8 @@
+package com.phantom.asalama.javajokes;
+
+public class Joker {
+    public String getJoke(){
+        return "Did you hear about the mathematician who's afraid of negative numbers ? "
+                +"He'll stop at nothing to avoid them";
+    }
+}
